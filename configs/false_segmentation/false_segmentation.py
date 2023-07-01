b125de5a5594d5a41689fdac72cf6ea603dd8f63
Vaczzy <1069166224@qq.com>
2023-07-01 16:09:46 +0800

    update

--- configs/false_segmentation/false_segmentation.py
+++
@@ -1,7 +0,0 @@
-_base_ = [
-    '../_base_/models/false_segmodel.py', '../_base_/datasets/segmentation_data.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_1300.py'
-]
-# potsdam example:
-model = dict(
-    decode_head=dict(num_classes=7), auxiliary_head=dict(num_classes=7))
